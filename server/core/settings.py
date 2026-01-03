d9abf5e974f4e6cb75bd67193433ea9211920287
Luca <auth@luca.cx>
2026-01-03 18:06:47 +0200

    Update CORS_ALLOWED_ORIGINS

--- server/core/settings.py
+++ server/core/settings.py
@@ -61,7 +61,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:5173"]
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:3000",
+    "https://readablock.com"
+]
 
 ROOT_URLCONF = 'core.urls'
 
